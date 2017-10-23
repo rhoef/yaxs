d5a4c3123e1cc8cd54c1d502b8bfa60bd5a82e6d
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2017-10-23 14:57:57 +0200

    VC

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+"""
+setup.py
+"""
+
+__author__ = 'rudolf.hoefler@gmail.com'
+__copyright__ = 'WTFL'
+
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'yaxs',
+    version = '1.0',
+    author = 'Rudolf Hoefler',
+    author_email = 'rudolf.hoefler@gmail.com.com',
+    description = ('Yet Another Xml Serializer'),
+    license = 'WTFL',
+    keywords = 'xml Serializer',
+    packages = find_packages(),
+)
